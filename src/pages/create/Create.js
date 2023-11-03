13a0ddae5545982ba954dc87c97e7768612b61aa
Mahsa Pesaran <mahsa.pesaran@gmail.com>
2023-11-03 11:50:04 +1100

    adding ingredients in the create template

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 // styles
 import './Create.css'
@@ -8,10 +8,25 @@ export default function Create() {
 const [title , setTitle] = useState('')
 const [method, setMethod] = useState('')
 const[cookingTime , setCookingTime] = useState('')
+const [newIngredients, setNewIngredients] = useState('') //adding new ingredient
+const [ingredients , setIngredients] = useState([]) //array to keep ingredients list
+const ingredientInput = useRef(null)
 
 const handleSubmit = (e)=> {
   e.preventDefault()
   console.log(title, method, cookingTime)
+  console.log(ingredients)
+}
+
+const handleadd = (e) => {
+  e.preventDefault()
+  const ing = newIngredients.trim()
+
+  if (ing && !ingredients.includes(ing)) {  //checks if the new value entered and also checks the array for the new ingredient be unique
+    setIngredients(prevIngredients => [...prevIngredients, ing])
+  }
+  setNewIngredients('')
+  ingredientInput.current.focus()
 }
 
   return (
@@ -28,7 +43,19 @@ const handleSubmit = (e)=> {
           />
         </label>
 
-      {/*ingredients go here */}
+        <label>
+          <span>Recipe ingeredients: </span>
+          <div className="inggredients">
+            <input 
+              type="text"
+              onChange={(e) => setNewIngredients(e.target.value)}
+              value={newIngredients}
+              ref = {ingredientInput}
+            />
+            <button onClick={handleadd} className="btn">Add</button>
+          </div>
+        </label>
+        <p>Current Ingredients : {ingredients.map((i) => <em key = {i}>{i}, </em>)}</p> {/*when we have map all the items should have a key */}
 
         <label>
           <span>Recipe Methos:</span>
